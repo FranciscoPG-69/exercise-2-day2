d6de16b8aedfe7bd93a21fcbcf10dbbb360de3a0
FranciscoPG-69 <55768594+FranciscoPG-69@users.noreply.github.com>
2023-09-08 15:54:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#suma los dos digitos de un numero
+two_digit_number = input("Type a two digit number: ")
+
+#new_num_var = int(two_digit_number)
+#print( new_num_var)
+
+#print(type(new_num_var))
+a=int ((two_digit_number[0]))
+b=int ((two_digit_number[1]))
+print(a+b)
